88a547e9129121b18a2f810098d2bc7e4895c21a
Anastasiya <antonio.223@inbox.ru>
2021-02-05 13:48:20 +0500

    Отоброжение поля игры и фигур на экране

--- src/game.js
+++ src/game.js
@@ -1,129 +1,143 @@
-export default class game{
-        core = 0; //колличестов очков
-        lines = 0; //сколько линий удалять
-        level = 0; //уровни
-        playfild = [//поле игры размером 10х20 будет предстваленно двумя массивами
-            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-            
-        ]; 
-        
-        // Активная фигура.
-        activePiece = {
-            x: 0,
-            y: 0,
-            blocks: [
+export default class game {
+    core = 0; //колличестов очков
+    lines = 0; //сколько линий удалять
+    level = 0; //уровни
+    playfild = this.createPlayfild(); //поле игры размером 10х20 будет предстваленно двумя массивами
+
+
+    // Активная фигура.
+    activePiece = {
+        x: 0,
+        y: 0,
+        blocks: [
             [0, 1, 0],
             [1, 1, 1],
             [0, 0, 0]
-            ]
-        };
+        ]
+    };
 
-        movePieceLeft(){
-            this.activePiece.x -= 1;
+    getState() {
+        const { y: pieceY, x: pieceX, blocks } = this.activePiece;
+        const playfild = this.createPlayfild(); 
 
-            if(this.isPieceOutOfBounds()){  //не даёт выйти фигуре из поля
-                this.activePiece.x +=1; 
+        for (let y = 0; y < this.playfild.length; y++) {
+            playfild[y] = [];
+
+            for (let x = 0; x < this.playfild[y].length; x++) {
+                playfild[y][x] = this.playfild[y][x];
             }
-        } 
+        }
+        for (let y = 0; y < blocks.length; y++) {
+            for (let x = 0; x < blocks[y].length; x++) {
+                if (blocks[y][x]) {
+                    playfild[pieceY + y][pieceX + x] = blocks[y][x];
+                }
 
-        movePieceRight(){
+            }
+
+        }
+        return { playfild };
+    }
+
+    createPlayfild() { //создает массив для игрового поля
+        const playfild = [];
+
+        for (let y = 0; y < 20; y++) {
+            playfild[y] = [];
+
+            for (let x = 0; x < 10; x++) {
+                playfild[y][x] = 0;
+            }
+        }
+        return playfild;
+    }
+    movePieceLeft() { // сдвигает фигуру влево
+        this.activePiece.x -= 1;
+
+        if (this.isPieceOutOfBounds()) {  //не даёт выйти фигуре из поля
             this.activePiece.x += 1;
-            
-            if(this.isPieceOutOfBounds()){
-                this.activePiece.x -= 1; 
-            }
-        } 
+        }
+    }
 
-        movePieceDown(){
-            this.activePiece.y += 1;
-    
-            if(this.isPieceOutOfBounds()){
-                this.activePiece.y -= 1; 
-                this.lockPiece();
-            }
-        } 
-        rotatePiece(){
-            const blocks = this.activePiece.blocks;
-            const length = blocks.length;
+    movePieceRight() {// сдивгает фигуру вправо
+        this.activePiece.x += 1;
 
-            const temp = [];
-            for (let i = 0; i < length; i++){
-                temp[i] = new Array(length).fill(0);
-            }
+        if (this.isPieceOutOfBounds()) {
+            this.activePiece.x -= 1;
+        }
+    }
 
-            for (let y = 0; y < length; y++){
-                for (let x = 0; x < length; x++)
+    movePieceDown() { //сдвигает фигуру вниз
+        this.activePiece.y += 1;
+
+        if (this.isPieceOutOfBounds()) {
+            this.activePiece.y -= 1;
+            this.lockPiece();
+        }
+    }
+    rotatePiece() { // поворачивает фигуру
+        const blocks = this.activePiece.blocks;
+        const length = blocks.length;
+
+        const temp = [];
+        for (let i = 0; i < length; i++) {
+            temp[i] = new Array(length).fill(0);
+        }
+
+        for (let y = 0; y < length; y++) {
+            for (let x = 0; x < length; x++)
                 temp[x][y] = blocks[length - 1 - y][x];
-            }
-
-            this.activePiece.blocks = temp;
-
-            if(this.hasCollision()){
-                this.activePiece.blocks = blocks;
-            }
-        }
-        hasCollision(){
-            const {y: pieceY, x: pieceX, blocks} = this.activePiece;
-
-            for(let y = 0; y < blocks.length; y++){
-                for(let x = 0; x < blocks[y].length; x++){  
-                    if  ( blocks[y][x]  && 
-                        (( this.playfild[pieceY + y] === undefined ||this.playfild[pieceY + y][pieceX + x] === undefined ) ||
-                         this.playfild[pieceY + y][pieceX + x])){
-                        return true;
-                    }
-                }
-            }
-            
-            return false;
-        }
-        isPieceOutOfBounds(){ //метод, который определет положение фигуры в поле
-            const {y: pieceY, x: pieceX, blocks} = this.activePiece;
-
-            for(let y = 0; y < blocks.length; y++){
-                for(let x = 0; x < blocks[y].length; x++){  
-                    if  ( blocks[y][x]  && //наличие блока в фигуре
-                        (( this.playfild[pieceY + y] === undefined ||this.playfild[pieceY + y][pieceX + x] === undefined ) ||
-                         this.playfild[pieceY + y][pieceX + x])){
-                        return true;
-                    }
-                }
-            }
-            
-            return false;
         }
 
-        lockPiece(){
-            const {y: pieceY, x: pieceX, blocks} = this.activePiece;
+        this.activePiece.blocks = temp;
 
-            for(let y = 0; y < blocks.length; y++){ //переберает ряды фигуры
-                for(let x = 0; x < blocks[y].length; x++){ // перебирает внутри рядов 
-                    if (blocks[y][x]){
-                        this.playfild[pieceY+ y][pieceX + x] = blocks[y][x];
-                    }   
+        if (this.hasCollision()) {
+            this.activePiece.blocks = blocks;
+        }
+    }
+    hasCollision() {
+        const { y: pieceY, x: pieceX, blocks } = this.activePiece;
+
+        for (let y = 0; y < blocks.length; y++) {
+            for (let x = 0; x < blocks[y].length; x++) {
+                if (blocks[y][x] &&
+                    ((this.playfild[pieceY + y] === undefined || this.playfild[pieceY + y][pieceX + x] === undefined) ||
+                        this.playfild[pieceY + y][pieceX + x])) {
+                    return true;
                 }
             }
         }
-    } 
-   
-        
+
+        return false;
+    }
+    isPieceOutOfBounds() { //метод, который определет положение фигуры в поле
+        const { y: pieceY, x: pieceX, blocks } = this.activePiece;
+
+        for (let y = 0; y < blocks.length; y++) {
+            for (let x = 0; x < blocks[y].length; x++) {
+                if (blocks[y][x] && //наличие блока в фигуре
+                    ((this.playfild[pieceY + y] === undefined || this.playfild[pieceY + y][pieceX + x] === undefined) ||
+                        this.playfild[pieceY + y][pieceX + x])) {
+                    return true;
+                }
+            }
+        }
+
+        return false;
+    }
+
+    lockPiece() {
+        const { y: pieceY, x: pieceX, blocks } = this.activePiece;
+
+        for (let y = 0; y < blocks.length; y++) { //переберает ряды фигуры
+            for (let x = 0; x < blocks[y].length; x++) { // перебирает внутри рядов 
+                if (blocks[y][x]) {
+                    this.playfild[pieceY + y][pieceX + x] = blocks[y][x];
+                }
+            }
+        }
+    }
+}
+
+
 
